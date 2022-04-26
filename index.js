6cfa77d8a23ab9aace378685c450b0b3ecbea33f
ricardoalexandrepereira <ricardo_certe@hotmail.com>
2022-04-26 14:15:57 +0100

    connect db

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const connectToDb = require("./database/db")
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectToDb();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`servidor rodando em http://localhost:${port}`);
+});
